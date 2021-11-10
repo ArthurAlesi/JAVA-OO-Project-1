d1aa697bd63427d3ea1e863131a84723720804be
Arthur Alesi Souza <54421573+ArthurAlesi@users.noreply.github.com>
2021-11-10 15:19:26 -0300

    Update Email.java

--- project/src/project/Email.java
+++ project/src/project/Email.java
@@ -1,9 +1,18 @@
 package project;
 
 public class Email {
+	public String nome;
+	public String[] destinatarios;
+	public String mensagem;
+	public String assunto;
+	
 
 	// TODO passar os nomes para inglês
-	Email(String string, String[] stringArray, String assunto, String mensagem){
-		
+	Email(String nome, String[] destinatarios, String assunto, String mensagem){
+		this.nome =nome;
+		this.destinatarios = destinatarios;
+		this.assunto = assunto;
+		this.mensagem = mensagem;
+			
 	}
 }
